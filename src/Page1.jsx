4d085b1bdcf73684c70e2eef6b5cb3e21d0934b6
Alexander-D-28 <justalexander28@gmail.com>
2026-02-02 12:37:10 -0500

    Updates
    A few basic syntax examples for JSX

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -3,7 +3,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const [count2, setCount2] = useState(0);
+  const [count2, setCount2] = useState(1000);
 
   return (
     <>
@@ -14,10 +14,13 @@ function App() {
           Hello team 7 SE group
         </p>
 
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 1)} style={{backgroundColor: 'blue', color: 'white'}}>
           You have clicked me: {count} times
         </button>
-        
+        <button onClick={() => setCount(0)} style={{backgroundColor: 'red', color: 'white'}}>
+          Reset Counter
+        </button>
+
         <div style={{marginTop: '500px'}}>
           <button onClick={() => setCount2((count2) => count2 + 1)}>
             You have clicked not_me: {count2} times
