45a270465a7385ea193146d3739f6e4264b6d5b7
WayOfDro <wayofdro@gmail.com>
2026-03-27 03:48:59 -0400

    Added Dark/Light Mode Functionality
    Added button for toggling theme into App.jsx, added the specific color formatting into index.css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Page1 from "./Page1";
 import MainPage from "./Pages/MainPage";
@@ -6,6 +6,38 @@ import TransactionPage from "./Pages/TransactionPage";
 import AboutPage from "./Pages/AboutPage";
 
 function App() {
+
+    //DARK & LIGHT MODE STUFF
+    //Holds current themem state, light or dark
+    const [theme, setTheme] = useState("dark");
+
+    // ON LOAD
+    //Check if user has stored theme, use that over default
+    useEffect(() => {
+      const savedTheme = localStorage.getItem("theme");
+      if (savedTheme) setTheme(savedTheme);
+    }, []);
+
+    // Whenever the themem is updated (user presses button), apply change to <body>, then store that state 
+    // to load next time user loads app
+    useEffect(() => {
+      document.body.className = theme;
+      localStorage.setItem("theme", theme);
+     }, [theme]);
+
+    // Toggle that the button works, swaps between light and dark
+    const toggleTheme = () => {
+      setTheme((prev) => {
+        if (prev === "dark") {
+          return "light";
+        } else {
+          return "dark";
+        }
+      });
+    };
+
+
+
 
     //React state array
     const [people, setPeople] = useState([
@@ -16,14 +48,32 @@ function App() {
       {id: 4, name: "Greg", total: 130.67},
     ]);
 
+    // RENDERING
+    //Determine which emoj to show, moon if in dark mode, sun if in light
+    let icon;
+    if (theme === "dark") {
+      icon = "🌙";
+    } else {
+      icon = "☀️";
+    }
 
   return (
+    <>
+    {/* Global toggle button */}
+    <button
+      onClick={toggleTheme}
+      style={{ position: "absolute", top: 55, right: 75 }}
+    >
+      {icon}
+    </button>
+
     <Routes>
       <Route path="/" element={<Page1 />} />
       <Route path="/main" element={<MainPage people={people} setPeople={setPeople}/>} />
       <Route path="/transaction" element={<TransactionPage />} />
       <Route path="/readme" element={<AboutPage />} />
     </Routes>
+    </>
   );
 }
 
